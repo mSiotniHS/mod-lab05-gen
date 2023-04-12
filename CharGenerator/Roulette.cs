10bf98be74e52a76d5f7e2111901307453fa2a43
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-12 22:50:18 +0300

    [feat] Реализовал `Roulette` для рандома с вероятностями

---
+++ CharGenerator/Roulette.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharGenerator
+{
+	public static class Roulette
+	{
+		private static readonly Random Random = new Random();
+
+		public static T Spin<T>(IList<T> items, IList<double> weights, out int idx)
+		{
+			if (items.Count != weights.Count)
+			{
+				throw new ArgumentException("Количества item-ов и весов должны совпадать");
+			}
+
+			var totalWeight = weights.Sum();
+
+			var randomNum = Random.NextDouble() * totalWeight;
+			var currentSector = 0.0;
+
+			for (var i = 0; i < items.Count; i++)
+			{
+				currentSector += weights[i];
+				if (randomNum <= currentSector)
+				{
+					idx = i;
+					return items[i];
+				}
+			}
+
+			throw new Exception("Случайное число оказалось вне отрезка");
+		}
+
+		public static T Spin<T>(IList<T> items, IList<double> weights) => Spin(items, weights, out _);
+	}
+}
