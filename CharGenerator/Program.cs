660a77d472281740976fba2b0c21853016323108
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-13 17:41:55 +0300

    [feat] Привёл пример программы

---
+++ CharGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CharGenerator
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			var alphabet = File
+				.ReadAllText(@".\assets\alphabet.txt")
+				.TrimEnd(Environment.NewLine.ToCharArray())
+				.ToCharArray();
+
+			var frequencyMatrix = File
+				.ReadAllText(@".\assets\bigram_frequency_table.txt")
+				.TrimEnd(Environment.NewLine.ToCharArray())
+				.Split(new[] {"\r\n", "\n"}, StringSplitOptions.None)
+				.Select(row => row
+					.Split(' ')
+					.Select(double.Parse)
+					.ToArray())
+				.ToArray();
+
+			var generator = new BigramBasedTextGenerator(alphabet, frequencyMatrix, 'а');
+
+			for (var i = 0; i < 1000; i++)
+			{
+				Console.Write(generator.Next());
+			}
+		}
+	}
+}
