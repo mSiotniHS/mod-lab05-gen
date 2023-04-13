9c3fae43ef5ec8949a5e68374aadcaa3817fda8b
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-13 17:41:22 +0300

    [feat] Добавил генератор текста на основе биграмм

---
+++ CharGenerator/BigramBasedTextGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CharGenerator
+{
+	public sealed class BigramBasedTextGenerator
+	{
+		private readonly char[] _alphabet;
+		private readonly double[][] _frequencyMatrix;
+
+		private int _previousIdx;
+
+		public BigramBasedTextGenerator(char[] alphabet, double[][] frequencyMatrix, char firstLetter)
+		{
+			_alphabet = alphabet;
+			_frequencyMatrix = frequencyMatrix;
+			_previousIdx = Array.IndexOf(_alphabet, firstLetter) switch
+			{
+				-1 => throw new Exception("Символ не из данного алфавита"),
+				var value => value
+			};
+		}
+
+		public char Next() =>
+			Roulette.Spin(_alphabet, _frequencyMatrix[_previousIdx], out _previousIdx);
+	}
+}
