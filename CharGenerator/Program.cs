f3a1113a9f201fe01177c20551ebfdc9c9389596
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-20 20:54:42 +0300

    [App] Обновил пример программы

--- CharGenerator/Program.cs
+++ CharGenerator/Program.cs
@@ -8,11 +8,20 @@ namespace CharGenerator
 	{
 		private const string OutDir = @".\out";
 		private static readonly string Task1File = Path.Combine(OutDir, "task1.txt");
+		private static readonly string Task2File = Path.Combine(OutDir, "task2.txt");
+		private static readonly string Task3File = Path.Combine(OutDir, "task3.txt");
 
 		private static void Main()
 		{
 			Directory.CreateDirectory(OutDir);
 
+			Task1();
+			Task2();
+			Task3();
+		}
+
+		private static void Task1()
+		{
 			var alphabet = File
 				.ReadAllText(@".\assets\alphabet.txt")
 				.TrimEnd(Environment.NewLine.ToCharArray())
@@ -29,7 +38,49 @@ namespace CharGenerator
 				.ToArray();
 
 			var generator = new BigramBasedTextGenerator(alphabet, frequencyMatrix);
-			File.WriteAllText(Task1File, generator.GenerateText(1200, 'б'));
+			File.WriteAllText(Task1File, generator.GenerateText(1200, 'я'));
+
+		}
+
+		private static void Task2()
+		{
+			var rows = File
+				.ReadAllText(@".\assets\word_frequency_table.txt")
+				.TrimEnd(Environment.NewLine.ToCharArray())
+				.Split(new[] {"\r\n", "\n"}, StringSplitOptions.None)
+				.Select(row =>
+				{
+					var split = row.Split(',');
+					return (split[0], int.Parse(split[1]));
+				})
+				.ToArray();
+
+			var words = rows.Select(row => row.Item1).ToArray();
+			var weights = rows.Select(row => row.Item2).ToArray();
+
+			var generator = new FrequencyBasedTextGenerator(words, weights);
+			File.WriteAllText(Task2File, generator.GenerateText(1000));
+		}
+
+		private static void Task3()
+		{
+			var rows = File
+				.ReadAllText(@".\assets\word_pair_frequency_table.txt")
+				.TrimEnd(Environment.NewLine.ToCharArray())
+				.Split(new[] {"\r\n", "\n"}, StringSplitOptions.None)
+				.Select(row =>
+				{
+					var split = row.Split(',');
+					return (split[0], int.Parse(split[1]));
+				})
+				.ToArray();
+
+			var pairs = rows.Select(row => row.Item1).ToArray();
+			var weights = rows.Select(row => row.Item2).ToArray();
+
+			var generator = new FrequencyBasedTextGenerator(pairs, weights);
+			File.WriteAllText(Task3File, generator.GenerateText(500));
+
 		}
 	}
 }
