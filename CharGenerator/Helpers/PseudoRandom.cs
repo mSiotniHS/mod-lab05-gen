fca1fd9afda5edb7bffa6d52864eea140c86bc33
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-20 21:38:22 +0300

    [feat] Написал `PseudoRandom` для тестирования

---
+++ CharGenerator/Helpers/PseudoRandom.cs
@@ -0,0 +1,16 @@
+﻿namespace CharGenerator.Helpers
+{
+	public sealed class PseudoRandom : IRandom
+	{
+		private readonly double[] _values;
+		private int _currentValueIdx;
+
+		public PseudoRandom(double[] values)
+		{
+			_values = values;
+			_currentValueIdx = 0;
+		}
+
+		public double NextDouble() => _values[_currentValueIdx++];
+	}
+}
